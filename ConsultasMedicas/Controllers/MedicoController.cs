9d1d361579e4d75a43bf3eb5b86a14b0ed50c2f7
Luis-60 <luiscarlosghfc@gmail.com>
2025-04-03 18:57:26 -0300

    Tela de Login e Cadastro Médico

--- ConsultasMedicas/Controllers/MedicoController.cs
+++ ConsultasMedicas/Controllers/MedicoController.cs
@@ -1,10 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
 
 namespace ConsultasMedicas.Controllers
 {
     public class MedicoController : Controller
     {
-        public IActionResult Create()
+        public IActionResult Registrar()
+        {
+            // Mock data for demonstration
+            ViewBag.Especialidade = new List<SelectListItem>
+            {
+                new SelectListItem { Value = "1", Text = "Cardiologia" },
+                new SelectListItem { Value = "2", Text = "Dermatologia" }
+            };
+
+            ViewBag.Consultorio = new List<SelectListItem>
+            {
+                new SelectListItem { Value = "1", Text = "Consultório A" },
+                new SelectListItem { Value = "2", Text = "Consultório B" }
+            };
+
+            return View();
+        }
+        public IActionResult Login()
         {
             return View();
         }
--- ConsultasMedicas/Models/Medico.cs
+++ ConsultasMedicas/Models/Medico.cs
@@ -6,12 +6,32 @@ namespace ConsultasMedicas.Models
     public class Medico
     {
         [Key]
-        [Required(ErrorMessage = "O campo Id é Obrigatório")]
         public int IdMedico { get; set; }
 
         [Required(ErrorMessage = "O campo Nome é Obrigatório")]
         [MaxLength(255)]
+        [Display(Name = "Nome")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo Telefone é Obrigatório")]
+        [MaxLength(255)]
+        [Display(Name = "Telefone")]
+        public string Telefone { get; set; }
+        [Required(ErrorMessage = "O campo Email é Obrigatório")]
+        [MaxLength(255)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "O campo CRM é Obrigatório")]
+        [MaxLength(255)]
+        [Display(Name = "CRM")]
+        public string CRM { get; set; }
+        [Required(ErrorMessage = "O campo CPF é Obrigatório")]
+        [MaxLength(16)]
+        [Display(Name = "CPF")]
+        public string CPF { get; set; }
+        [Required(ErrorMessage = "O campo Senha é Obrigatório")]
+        [MaxLength(48)]
+        [Display(Name = "Senha")]
+        public string Senha { get; set; }
         [Display(Name = "Consultório")]
         [ForeignKey("Consultorio")]
         public int idConsultorio { get; set; }
