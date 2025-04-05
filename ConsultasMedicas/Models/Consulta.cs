1345a740f4e822db5b72d7e41c5d6a864fecb95d
Torres3111 <phts2005@gmail.com>
2025-04-05 18:37:02 -0300

    Telas Consulta e Marcar consulta, SweetAlert, Datatable

--- ConsultasMedicas/Models/Consulta.cs
+++ ConsultasMedicas/Models/Consulta.cs
@@ -1,14 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using ConsultasMedicas.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 public class Consulta
 {
     [Key]
     public int IdConsulta { get; set; }
+
     [Display(Name = "Médico")]
     [ForeignKey("Medico")]
     public int IdMedico { get; set; }
+
+    [Display(Name = "Consultorio")]
+    [ForeignKey("Consultorio")]
+    public int IdConsultorio { get; set; }
+
     [Display(Name = "Cliente")]
     [ForeignKey("Cliente")]
     public int IdCliente { get; set; }
@@ -22,6 +29,12 @@ public class Consulta
     [Required(ErrorMessage = "O campo Data é Obrigatório")]
     [DataType(DataType.Date)]
     public DateTime Data { get; set; }
+    
     public virtual Medico? Medico { get; set; }
     public virtual Cliente? Cliente { get; set; }
+
+
+
+    
+
 }
---
+++ ConsultasMedicas/ViewModel/ConsultaViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+
+namespace SeuProjeto.ViewModels
+{
+    public class ConsultaViewModel
+    {
+        public Consulta Consulta { get; set; } = new Consulta();
+        public int IdMedico { get; set; }
+        public int IdEspecialidade { get; set; }
+        public int IdUF { get; set; }
+        public DateTime Data { get; set; }
+        public TimeSpan Horario { get; set; }
+
+        // Listas para os combos
+        public List<SelectListItem> UFs { get; set; } = new();
+        public List<SelectListItem> Especialidades { get; set; } = new();
+        public List<SelectListItem> Medicos { get; set; } = new();
+
+        public List<SelectListItem> Consultorios { get; set; } = new();
+
+        public List<SelectListItem> Horarios { get; set; } = new(); 
+    }
+}
